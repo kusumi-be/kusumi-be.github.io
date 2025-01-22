8c5664b13b3cf002fbf5a741980af96c911469ec
DESKTOP-GAJD88D\bell <ohya.w.tktm.tm@gmail.com>
2025-01-23 01:18:29 +0900

    change docusaurus.config.js "baseUrl"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://kusumi-be.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kusumi-be.github.io/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
